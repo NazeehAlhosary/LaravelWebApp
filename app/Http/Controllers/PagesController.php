6139a2b79e751300269e8694b268551ef272dadb
nazeehalhosary <nazeeh.substly@gmail.com>
2019-03-04 11:29:22 +0100

    Add Laravel config Add three pages and test page Put some styles |

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        $title = 'Welcome to Larave from Controller!';
+        //return view('pages.index',compact('title'));//way 1 
+        return view('pages.index')->with('title',$title);//way 2
+    }
+    public function test(){
+        $title = 'Welcome to Test!';
+        return view('pages.test')->with('title',$title);
+    }
+    public function about(){
+        $title = 'Welcome to About!';
+        return view('pages.about')->with('title',$title);
+    }
+    public function services(){
+       // $title = 'Welcome to Services!';
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web Design','Programming','ESO']
+        );
+        return view('pages.services')->with($data);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/users/{id}', function ($id) {
+    return 'Hello user '. $id;
+});
+
+Route::get('/users/{id}/{name}', function ($id,$name) {
+    return 'Hello user '. $name .', Your id is: ' .$id;
+});*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+*/
+Route::get('/test', 'PagesController@test');
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
+
+
